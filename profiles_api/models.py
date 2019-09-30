4def9eef3a01fb645566165344fe043545abdd73
adil.farhan <adilfarhan28@gmail.com>
2019-09-29 22:33:09 -0700

    Created db model with migrations and migrated the database

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,20 +1,45 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models her_e.
+class UserProfileManager(BaseUserManager):
+    """Manager for user profile"""
+
+    def create_user(self,name,email,password=None):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError('User must have an email address')
+
+        email = self.normalized_email(email)
+        user = self.model(email = email, name= name)
+        user = user.set_password(password)
+        user.save(using = self._db)
+
+        return user
+
+
+    def create_superuser(self,email,name,password):
+        """Create a super user"""
+        user = self.create_user(email,name,password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using = self._db)
+
+        return user
+
 
 class UserProfile(AbstractBaseUser,PermissionsMixin):
     """Database model for UserProfile in system"""
     email = models.EmailField(max_length = 255, unique = True)
     name = models.CharField(max_length = 255)
-    is_active = model.BooleanField(default = True)
-    is_staff = model.BooleanField(default = False)
+    is_active = models.BooleanField(default = True)
+    is_staff = models.BooleanField(default = False)
 
     objects = UserProfileManager()
 
+    REQUIRED_FIELDS = ['name']
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = 'name'
 
     def get_full_name(self):
         """Get full name of user """

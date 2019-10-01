3833521508fc64216f367556088a88d5366cfc86
adil.farhan <adilfarhan28@gmail.com>
2019-10-01 12:34:16 +0500

    Created and Check Views

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test Api View"""
+
+    def get(self,request,format=None):
+        """Returns a list of API Views"""
+        an_apiview = [
+        'Uses http method as a function (get, post, patch, delete,put)',
+        'Is smilar to traditional  Djaongo View',
+        'Give you the most control over application logic',
+        'Is mapped manually to URLS',
+        ]
+
+        return Response({'message':'Hello !','an_apiview': an_apiview} )
+        """ Dictionary is passed to this Response """

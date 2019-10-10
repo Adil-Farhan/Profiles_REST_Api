25c019d75e5d94ccc43952cf517c9e0e410fdbb5
adil.farhan <adilfarhan28@gmail.com>
2019-10-10 11:33:13 +0500

    authentication and permissions added to code

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,13 +1,16 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
 from rest_framework import viewsets
 
+from profiles_api import models
 from profiles_api import serializers
+from profiles_api import permissions
 
-class HelloApiView(APIView):
     """Test Api View"""
     serializer_class = serializers.HelloSerializers
+    class HelloApiView(APIView):
 
 
     def get(self,request,format=None):
@@ -95,3 +98,10 @@ class HelloViewSets(viewsets.ViewSet):
     def destroy(self,request,pk=None):
         """ Handle destroy update object by Id"""
         return Response({'http_method':'DELETE'})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle and Create/Updating profiles"""
+    serializer_class =  serializers.UserProfileSerailizer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
